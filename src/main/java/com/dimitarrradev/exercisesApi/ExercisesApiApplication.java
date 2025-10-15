c00b53b767d961c5a6d136863371589be8626ded
DimitarRadev88 <dimitar.rumenov.radev@gmail.com>
2025-10-15 18:06:55 +0300

    initial commit

---
+++ src/main/java/com/dimitarrradev/exercisesApi/ExercisesApiApplication.java
@@ -0,0 +1,13 @@
+package com.dimitarrradev.exercisesApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExercisesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExercisesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dimitarrradev/exercisesApi/ExercisesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dimitarrradev.exercisesApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExercisesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
